158f11f53f0d420bb2edc7f3331048a126603195
john <lord1229@gmail.com>
2021-11-22 13:09:25 +0900

    docker mysql 이미지에 user 스키마 변경 및 테스트 user 추가

--- docker-image/mysql/docker-entrypoint-initdb.d/2-user.sql
+++ docker-image/mysql/docker-entrypoint-initdb.d/2-user.sql
@@ -4,9 +4,8 @@ CREATE TABLE USER (
   `EMAIL` varchar(64) DEFAULT NULL COMMENT '이메일',
   `PASSWORD` varchar(64) DEFAULT NULL COMMENT '비밀번호',
   `NAME` varchar(8) DEFAULT NULL COMMENT '이름',
-  `FAV_DIRECTORS` varchar(64) DEFAULT NULL COMMENT '좋아하는 감독들, 스페이스로 구분',
-  `FAV_ACTORS` varchar(64) DEFAULT NULL COMMENT '좋아하는 배우, 스페이스로 구분',
-  `FAV_GENRES` varchar(64) DEFAULT NULL COMMENT '좋아하는 장르, 스페이스로 구분',
+  `ATTR1` varchar(8) DEFAULT NULL COMMENT '특성1',
+  `ATTR2` varchar(8) DEFAULT NULL COMMENT '특성2',
   `REG_DATE` datetime COMMENT '생성일자',
   `MOD_DATE` datetime COMMENT '수정일자'
 );
